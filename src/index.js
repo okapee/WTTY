8c3c4c5c1a1b04b61cb92d2b7f50ae5f3de455c1
Masahiro Okazaki <okazaki.masahiro@fastaccounting.co.jp>
2021-11-12 11:56:26 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 
 ReactDOM.render(
   <React.StrictMode>
